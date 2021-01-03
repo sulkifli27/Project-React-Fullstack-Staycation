032a7a06fbbae6e6da5d784ffbecdaeab6606031
sulkifli <sulgameranimasi1@gmail.com>
2021-01-03 13:54:30 -0800

    finis component categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
   constructor(props) {
@@ -9,7 +10,6 @@ export default class LandingPage extends Component {
     this.refMostPicked = React.createRef();
   }
   render() {
-    console.log(this.props);
     return (
       <>
         <Header {...this.props}></Header>;
@@ -18,6 +18,7 @@ export default class LandingPage extends Component {
           refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked}
         />
+        <Categories data={landingPage.categories} />
       </>
     );
   }
